cb6db9a3d2635fcc4befd4268f1301717e3b0765
VM_Lodoz <kiporovv1@gmail.com>
2023-04-26 01:31:45 +0700

    Program ready

---
+++ MiAPO/Main.cs
@@ -0,0 +1,39 @@
+﻿namespace MiAPO
+{
+    public partial class Practos
+    {
+        public static void Main()
+        {
+            float numeric1 = 0;
+            float numeric2 = 0;
+            char operation;
+
+            Console.WriteLine("Введите первое число");
+            numeric1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            numeric2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: +, -, /, *");
+            operation = char.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case '+':
+                    Adder(numeric1, numeric2);
+                    break;
+
+                case '-':
+                    Subtractor(numeric1, numeric2);
+                    break;
+
+                case '/':
+                    Divider(numeric1, numeric2);
+                    break;
+
+                case '*':
+                    Multiplier(numeric1, numeric2);
+                    break;
+            }
+
+        }
+    }
+}
---
+++ MiAPO/Methods/Subtractor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiAPO
+{
+    public partial class Practos
+    {
+        public static void Subtractor(float number1, float number2)
+        {
+            Console.Clear();
+            Console.WriteLine($"{number1} - {number2} = {number1 - number2}");
+        }
+    }
+}
